349fc53816790849e1030c58c4ca9e66753500a0
andrejonat <112368139+andrejonat@users.noreply.github.com>
2022-09-09 09:43:57 +0700

    Add files via upload
    UPRAK Andre X TKJ 3

---
+++ UPRAK_ANDRE_X TKJ 3.py.py
@@ -0,0 +1,8 @@
+# Balok
+
+panjang = int(input("masukkan nilai panjang:"))
+lebar = int(input("masukkan nilai lebar:"))
+tinggi = int(input("masukkan nilai tinggi:"))
+volume_balok = panjang * lebar * tinggi
+
+print("volume balok adalah",volume_balok)
